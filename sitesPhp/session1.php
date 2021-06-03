900a1af3a2fa6fda589a47ffef747b6f0d70543d
Anthony <anthony.rolland@etu.uca.fr>
2021-06-03 12:10:51 +0200

    tp sur session

--- sitesPhp/session1.php
+++ sitesPhp/session1.php
@@ -1,16 +1,19 @@
 <?php
-    session_id("c3c1e5tun1dd3s3s51on");
-    session_start();
+    $a = session_id("c3c1e5tun1dd3s3s51on");
+    if(empty($a)) session_start();
 ?>
+
 <div style="display: flex; margin: auto; flex-direction: column; width: 100%; align-items: center">
     <h1>Level 1</h1>
-    <form style="display: flex; flex-direction: column">
-        <?php
-            session_start();
-            echo $_SESSION;
-        ?>
+    <p>Retrouver l'id de session. Plusieurs solution sont possible</p>
+    <form name="s1" action="session1.php" method="POST"
+          style="display: flex; flex-direction: column">
+        <input name="msg" placeholder="Votre message"/>
+        <button type="submit" style="margin-top: 0.25rem;">Envoyer</button>
     </form>
+    <div style="display: flex; margin: auto; flex-direction: column; width: 100%; align-items: center">
+        <?php
+            echo "<p>".$_POST['msg']."</p>";
+        ?>
+    </div>
 </div>
-
-<?php
-    echo $_SESSION;
