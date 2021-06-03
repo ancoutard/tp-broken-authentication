ecb7094c5d9f3015655d3e3cdc74077be012baef
Anthony Rolland <anthony.rlld@gmail.com>
2021-06-03 12:25:58 +0200

    Partie session revue

--- sitesPhp/session.php
+++ sitesPhp/session.php
@@ -1,9 +1,20 @@
+<?php
+$a = session_id("c3c1e5tun1dd3s3s51on");
+if(empty($a)) session_start();
+?>
+
 <div style="display: flex; margin: auto; flex-direction: column; width: 100%; align-items: center">
-    <h1>Session</h1>
-    <div style="display: flex; flex-direction: column">
-        <a href="session1.php" style="margin: 0.25rem">Level 1</a>
-        <a href="session2.php" style="margin: 0.25rem">Level 2</a>
+    <h1>Level 1</h1>
+    <p>Trouvez deux solutions pour retrouvez l'identifiant de session.</p>
+    <p>Le formulaire peut vous aider.</p>
+    <form name="s1" action="session1.php" method="POST"
+          style="display: flex; flex-direction: column">
+        <input name="msg" placeholder="Votre message"/>
+        <button type="submit" style="margin-top: 0.25rem;">Envoyer</button>
+    </form>
+    <div style="display: flex; margin: auto; flex-direction: column; width: 100%; align-items: center">
+        <?php
+            echo "<p>".$_POST['msg']."</p>";
+        ?>
     </div>
 </div>
-
-<?php
--- sitesPhp/session2.php
+++
@@ -1,9 +0,0 @@
-<div style="display: flex; margin: auto; flex-direction: column; width: 100%; align-items: center">
-    <h1>Level 2</h1>
-    <div style="display: flex; flex-direction: column">
-        <a href="session1.php" style="margin: 0.25rem">Level 1</a>
-        <a href="session2.php" style="margin: 0.25rem">Level 2</a>
-    </div>
-</div>
-
-<?php
